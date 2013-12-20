02f2bbdcdbdf033a10b6b2c49f4a621e190957df
devkyron <abhigyan.raghav@kyron.me>
2013-12-20 12:52:49 +0530

    Project created

---
+++ disco/disco/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  disco
+//
+//  Created by Abhigyan Raghav on 12/20/13.
+//  Copyright (c) 2013 Kyron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ disco/disco/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  disco
+//
+//  Created by Abhigyan Raghav on 12/20/13.
+//  Copyright (c) 2013 Kyron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
